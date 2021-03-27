17fb905b69db30f69ad33c17bc0702e8bc5cea2a
Matthew Jiao <jiaomatthew222@gmail.com>
2021-03-27 12:09:50 -0400

    demo component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -41,9 +41,7 @@ const Hero = (props) => {
         </div>
         </Holder2>
         </InnerWrapper>
-        <DemoVideo>
-
-        </DemoVideo>
+        
         
       </Wrapper>
     </Container>
